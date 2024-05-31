a7be00b554420baa9f442be39ed34f88b68bcdaa
dwijgohil <20ecuot047@ddu.ac.in>
2024-06-01 01:15:28 +0530

    implemented update and delete task functionalties

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 # Create your views here.
@@ -20,3 +20,15 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__' #as i want all the fields as per the modal to be in the form.
     success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__' #as i want all the fields as per the modal to be in the form.
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
