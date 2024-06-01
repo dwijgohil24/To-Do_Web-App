f5a79d6e689243192f67484f510ace2dec733d8d
dwijgohil <20ecuot047@ddu.ac.in>
2024-06-01 15:56:50 +0530

    added new user register functionality.

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate, TaskDelete, customLoginView
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate, TaskDelete, customLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', customLoginView.as_view(), name = 'login'),
     path('logout/', LogoutView.as_view(next_page = 'login'), name = 'logout'),
+    path('register/', RegisterPage.as_view(), name = 'register'), 
 
     path('',TaskList.as_view(), name = 'tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(), name = 'task'),
--- base/views.py
+++ base/views.py
@@ -1,26 +1,57 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Task
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login 
+
 # Create your views here.
 
 class customLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
-    redirect_authenticated_user = True
+    redirect_authenticated_user = True #to only redirect the authenticated user
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirected_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage,self).get(*args, **kwargs)
+    
+
+
     
 #we are inheriting from the ListView to make our class TaskList, we will have all the functionality list view has and on top of it we will make our custom additional functionalities.
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user = self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
@@ -29,17 +60,22 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__' #as i want all the fields as per the modal to be in the form.
+    fields = ['title', 'description', 'complete'] #as i want all the fields as per the modal to be in the form.
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+    
 
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__' #as i want all the fields as per the modal to be in the form.
+    fields = ['title', 'description', 'complete'] #as i want all the fields as per the modal to be in the form.
     success_url = reverse_lazy('tasks')
 
 class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
-    context_object_name = 'task'
+    context_object_name = 'tasks'
     success_url = reverse_lazy('tasks')
 
